46452d3d22597fea798f964f023384f7d7efd7b9
nicholaschangIT <nicholasc001@hotmail.com>
2025-01-25 01:04:39 +0000

    fix public ip allocation

--- terraform/vnet.tf
+++ terraform/vnet.tf
@@ -16,7 +16,8 @@ resource "azurerm_public_ip" "pub" {
   name                = "pip-azurespringclean2025"
   location            = var.location
   resource_group_name = azurerm_resource_group.this.name
-  allocation_method   = "Dynamic"
+  allocation_method   = "Static"
+  sku                 = "Standard"
 }
 
 resource "azurerm_network_interface" "nic" {
