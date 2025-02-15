fb5450eac57957845a27a75409d26f80a1c3db77
nicholaschangIT <nicholasc001@hotmail.com>
2025-02-15 13:35:47 +0000

    edit pipeline

--- pipeline-scripts/sandbox-override.sh
+++ pipeline-scripts/sandbox-override.sh
@@ -2,13 +2,16 @@
 set -ex
 
 # Define directories
-export SUB_ASSIGNMENTS=$(find ./assignments/$SUB -type f -name 'assign.*.json')
-export POLICIES=$(find ./policies -name '*.json' -type f)
+# Find all assignment files in the specified subscription directory
+export SUB_ASSIGNMENTS=$(find ${assignmentsDirectory}/$SUB -type f -name 'assign.*.json')
+# Find all policy files in the policies directory
+export POLICIES=$(find ${policiesDirectory} -name '*.json' -type f)
 
 # Deploy policies
 echo "Deploying Policies"
 for policy in ${POLICIES}; do
     echo "Deploying policy: ${policy}"
+    # Create policy definition using the Azure CLI
     az policy definition create --name $(basename ${policy} .json) --rules ${policy} --mode All --display-name $(basename ${policy} .json) --description "Policy from ${policy}"
 done
 
@@ -17,5 +20,6 @@ echo "Deploying Subscription Assignments"
 for assignment in ${SUB_ASSIGNMENTS}; do
     echo "Deploying policy assignment: ${assignment}"
     assignmentName=$(basename ${assignment} .json)
+    # Create policy assignment using the Azure CLI
     az policy assignment create --name ${assignmentName} --policy $(jq -r '.properties.policyDefinitionId' ${assignment}) --scope $(jq -r '.properties.scope' ${assignment}) --params $(jq -r '.properties.parameters' ${assignment})
 done
