e46b9679579c1c06316e26a1a71d219451d64df3
nicholaschangIT <nicholasc001@hotmail.com>
2025-02-17 22:40:43 +0000

    edit policy

--- pipeline-scripts/deploy-policies.sh
+++ pipeline-scripts/deploy-policies.sh
@@ -8,12 +8,13 @@ export ASSIGNMENTS_DIR="$3"
 echo "Deploying Policies"
 for policy in $(find ${POLICIES_DIR} -name '*.json' -type f); do
   policy_name=$(basename "${policy}" .json)
+  display_name=$(jq -r '.properties.displayName' "${policy}")
   echo "Validating policy: ${policy}"
   if jq -e '.properties.policyRule' "${policy}" >/dev/null && jq -e '.properties.parameters' "${policy}" >/dev/null; then
     policy_rule=$(jq -c '.properties.policyRule' "${policy}")
     parameters=$(jq -c '.properties.parameters' "${policy}")
-    echo "Creating policy definition: $policy_name"
-    az policy definition create --name $policy_name --rules "${policy_rule}" --params "${parameters}" --mode All --display-name "${policy_name}" --description "Policy from ${policy}"
+    echo "Creating policy definition: $display_name"
+    az policy definition create --name $policy_name --rules "${policy_rule}" --params "${parameters}" --mode All --display-name "${display_name}" --description "Policy from ${policy}"
   else
     echo "Error: Policy file ${policy} does not have a valid 'policyRule' or 'parameters' field."
     exit 1
@@ -29,4 +30,4 @@ for assignment in $(find ${ASSIGNMENTS_DIR} -type f -name 'assign.*.json'); do
 done
 
 echo "Triggering Compliance Scan"
-# az policy state trigger-scan --resource "/subscriptions/$SUB"
+az policy state trigger-scan --resource "/subscriptions/$SUB"
