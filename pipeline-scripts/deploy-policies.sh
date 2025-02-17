b13ad7a1410321c0987186954015bdd002f52d10
nicholaschangIT <nicholasc001@hotmail.com>
2025-02-17 22:23:24 +0000

    edit policy

--- pipeline-scripts/deploy-policies.sh
+++ pipeline-scripts/deploy-policies.sh
@@ -12,6 +12,7 @@ for policy in $(find ${POLICIES_DIR} -name '*.json' -type f); do
   if jq -e '.properties.policyRule' "${policy}" >/dev/null && jq -e '.properties.parameters' "${policy}" >/dev/null; then
     policy_rule=$(jq -c '.properties.policyRule' "${policy}")
     parameters=$(jq -c '.properties.parameters' "${policy}")
+    echo "Creating policy definition: $policy_name"
     az policy definition create --name $policy_name --rules "${policy_rule}" --params "${parameters}" --mode All --display-name "${policy_name}" --description "Policy from ${policy}"
   else
     echo "Error: Policy file ${policy} does not have a valid 'policyRule' or 'parameters' field."
@@ -23,9 +24,9 @@ echo "Deploying Subscription Assignments"
 for assignment in $(find ${ASSIGNMENTS_DIR} -type f -name 'assign.*.json'); do
   assignment_name=$(basename "${assignment}" .json)
   policy_definition_id=$(jq -r '.properties.policyDefinitionId' "${assignment}")
-  echo "Deploying assignment: ${assignment}"
+  echo "Creating policy assignment: $assignment_name"
   az policy assignment create --name $assignment_name --policy "${policy_definition_id}" --scope "/subscriptions/$SUB" --display-name "$assignment_name"
 done
 
-# echo "Triggering Compliance Scan"
-# az policy state trigger-scan --resource "/subscriptions/$SUB"
+echo "Triggering Compliance Scan"
+az policy state trigger-scan --resource "/subscriptions/$SUB"
