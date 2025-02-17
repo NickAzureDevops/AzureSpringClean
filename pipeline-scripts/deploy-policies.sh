8cc14929c9f0f3e00752739276da6ba16069b021
nicholaschangIT <nicholasc001@hotmail.com>
2025-02-17 01:32:35 +0000

    edit pipeline

---
+++ pipeline-scripts/deploy-policies.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -ex
+
+export SUB="$1"
+export POLICIES_DIR="$2"
+export ASSIGNMENTS_DIR="$3"
+
+echo "Deploying Policies"
+for policy in $(find ${POLICIES_DIR} -name '*.json' -type f); do
+  policy_name=$(basename "${policy}" .json)
+  echo "Validating policy: ${policy}"
+  if jq -e '.properties' "${policy}" >/dev/null; then
+    az policy definition create --name $policy_name --rules "${policy}" --mode All --display-name "${policy_name}" --description "Policy from ${policy}"
+  else
+    echo "Error: Policy file ${policy} does not have a valid 'properties' field."
+    exit 1
+  fi
+done
+
+echo "Deploying Subscription Assignments"
+for assignment in $(find ${ASSIGNMENTS_DIR} -type f -name 'assign.*.json'); do
+  assignment_name=$(basename "${assignment}" .json)
+  policy_definition_id=$(jq -r '.properties.policyDefinitionId' "${assignment}")
+  echo "Deploying assignment: ${assignment}"
+  az policy assignment create --name $assignment_name --policy "${policy_definition_id}" --scope "/subscriptions/$SUB" --display-name "$assignment_name"
+done
