9252fd69f74c9c5825983a2fa9edcc2af4f0ff11
nicholaschangIT <nicholasc001@hotmail.com>
2025-03-01 02:04:47 +0000

    convert to use avm for code

--- terraform/vm.tf
+++ terraform/vm.tf
@@ -8,21 +8,12 @@ module "vm" {
 
   # Resource Group
   resource_group_name = azurerm_resource_group.this.name
-  location            = var.location
-
-  # Network Interface
-  subnet_id           = azurerm_subnet.subnet.id
 
   # VM Configuration
   vm_os_simple        = "Windows2019"
   admin_username      = var.adminuser
   admin_password      = var.admin_password
   vm_size             = "Standard_B1s"
-  storage_os_disk     = {
-    name              = "azurespringclean-os-disk"
-    caching           = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
 
   # Tags
   tags = {
@@ -47,4 +38,25 @@ resource "azurerm_subnet" "subnet" {
   resource_group_name  = azurerm_resource_group.this.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_public_ip" "pub" {
+  name                = "pip-azurespringclean2025"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.this.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "nic-azurespringclean2025"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.this.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.pub.id
+  }
 }
