abe5c831afce5549892bf96c1348d103e6413ad3
nicholaschangIT <nicholasc001@hotmail.com>
2025-03-01 10:25:21 +0000

    fix avm code

--- terraform/vm.tf
+++ terraform/vm.tf
@@ -1,15 +1,19 @@
-module "vm" {
-  source  = "Azure/compute/azurerm"
-  version = "3.0.0"  # Use the latest version available
+provider "azurerm" {
+  features {}
+}
 
-  # Resource Group
+module "linuxservers" {
+  source              = "Azure/compute/azurerm"
+  version             = "3.0.0"  # Use the latest version available
   resource_group_name = azurerm_resource_group.this.name
 
   # VM Configuration
-  vm_os_simple        = "Windows2019"
+  vm_os_simple        = "UbuntuServer"
   admin_username      = var.adminuser
   admin_password      = var.admin_password
   vm_size             = "Standard_B1s"
+  public_ip_dns       = ["linsimplevmips"]
+  vnet_subnet_id      = azurerm_subnet.subnet.id
 
   # Tags
   tags = {
@@ -22,3 +26,16 @@ resource "azurerm_resource_group" "this" {
   location = var.location
 }
 
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.this.location
+  resource_group_name = azurerm_resource_group.this.name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "subnet"
+  resource_group_name  = azurerm_resource_group.this.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
--- terraform/vnet.tf
+++ terraform/vnet.tf
@@ -1,17 +1,3 @@
-resource "azurerm_virtual_network" "vnet" {
-  name                = "vnet"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.this.location
-  resource_group_name = azurerm_resource_group.this.name
-}
-
-resource "azurerm_subnet" "subnet" {
-  name                 = "subnet"
-  resource_group_name  = azurerm_resource_group.this.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.1.0/24"]
-}
-
 resource "azurerm_public_ip" "pub" {
   name                = "pip-azurespringclean2025"
   location            = var.location
