90a445c11dc054df1641c4a937d04b3c2f35d312
nicholaschangIT <nicholasc001@hotmail.com>
2025-03-01 10:10:06 +0000

    delete duplication code

--- terraform/vm.tf
+++ terraform/vm.tf
@@ -26,37 +26,3 @@ resource "azurerm_resource_group" "this" {
   location = var.location
 }
 
-resource "azurerm_virtual_network" "vnet" {
-  name                = "vnet"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.this.location
-  resource_group_name = azurerm_resource_group.this.name
-}
-
-resource "azurerm_subnet" "subnet" {
-  name                 = "subnet"
-  resource_group_name  = azurerm_resource_group.this.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.1.0/24"]
-}
-
-resource "azurerm_public_ip" "pub" {
-  name                = "pip-azurespringclean2025"
-  location            = var.location
-  resource_group_name = azurerm_resource_group.this.name
-  allocation_method   = "Static"
-  sku                 = "Standard"
-}
-
-resource "azurerm_network_interface" "nic" {
-  name                = "nic-azurespringclean2025"
-  location            = var.location
-  resource_group_name = azurerm_resource_group.this.name
-
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.subnet.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.pub.id
-  }
-}
