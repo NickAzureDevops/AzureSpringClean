4f91827bf720093e49d6f330e39c373fb57ec75b
nicholaschangIT <nicholasc001@hotmail.com>
2025-03-01 00:28:08 +0000

    remove pipeline to folder

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ const url = require('url');
 
 // Vulnerability: Hardcoded API key
 const apiKey = 'InstrumentationKey=248ccf4a-507c-4a7f-b392-0715dbe12cb7;IngestionEndpoint=https://uksouth-0.in.applicationinsights.azure.com/;LiveEndpoint=https://uksouth.livediagnostics.monitor.azure.com/;ApplicationId=8ad3e79d-5359-4635-872e-17664bf437b7';
+const apiKey2 = 'sv=2021-10-04&si=azureml-system-datastore-policy&sr=c&sig=6qEkidd3POtYJspd1RdsQk44LldgtXuWPe%2BGiY%2FthB4%3D';
+
+
 
 const server = http.createServer((req, res) => {
     const queryObject = url.parse(req.url, true).query;
