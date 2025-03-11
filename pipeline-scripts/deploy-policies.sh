b78ba48984fa18f3058a4aa126f87bd287c56bad
nicholaschangIT <nicholasc001@hotmail.com>
2025-03-11 20:23:27 +0000

    comment out policy code

--- pipeline-scripts/deploy-policies.sh
+++ pipeline-scripts/deploy-policies.sh
@@ -6,25 +6,25 @@ export POLICIES_DIR="$2"
 export ASSIGNMENTS_DIR="$3"
 
 # Function to deploy a single policy
-deploy_policy() {
-  local policy=$1
-  local policy_name=$(basename "${policy}" .json)
-  local display_name=$(jq -r '.properties.displayName' "${policy}")
+# deploy_policy() {
+#   local policy=$1
+#   local policy_name=$(basename "${policy}" .json)
+#   local display_name=$(jq -r '.properties.displayName' "${policy}")
 
-  echo "Validating policy: ${policy}"
-  if jq -e '.properties.policyRule' "${policy}" >/dev/null && jq -e '.properties.parameters' "${policy}" >/dev/null; then
-    local policy_rule=$(jq -c '.properties.policyRule' "${policy}")
-    local parameters=$(jq -c '.properties.parameters' "${policy}")
-    echo "Creating policy definition: $display_name"
-    az policy definition create --name $policy_name --rules "${policy_rule}" --params "${parameters}" --mode All --display-name "${display_name}" --description "Policy from ${policy}" || {
-      echo "Error: Failed to create policy definition: $display_name"
-      exit 1
-    }
-  else
-    echo "Error: Policy file ${policy} does not have a valid 'policyRule' or 'parameters' field."
-    exit 1
-  fi
-}
+#   echo "Validating policy: ${policy}"
+#   if jq -e '.properties.policyRule' "${policy}" >/dev/null && jq -e '.properties.parameters' "${policy}" >/dev/null; then
+#     local policy_rule=$(jq -c '.properties.policyRule' "${policy}")
+#     local parameters=$(jq -c '.properties.parameters' "${policy}")
+#     echo "Creating policy definition: $display_name"
+#     az policy definition create --name $policy_name --rules "${policy_rule}" --params "${parameters}" --mode All --display-name "${display_name}" --description "Policy from ${policy}" || {
+#       echo "Error: Failed to create policy definition: $display_name"
+#       exit 1
+#     }
+#   else
+#     echo "Error: Policy file ${policy} does not have a valid 'policyRule' or 'parameters' field."
+#     exit 1
+#   fi
+# }
 
 # Function to deploy a single assignment
 deploy_assignment() {
@@ -43,14 +43,14 @@ deploy_assignment() {
 }
 
 # Deploy all policies
-echo "Deploying Policies"
-for policy in $(find ${POLICIES_DIR} -name '*.json' -type f); do
-  deploy_policy "${policy}"
-done
+# echo "Deploying Policies"
+# for policy in $(find ${POLICIES_DIR} -name '*.json' -type f); do
+#   deploy_policy "${policy}"
+# done
 
 # Deploy all assignments
 echo "Deploying Subscription Assignments"
-for assignment in $(find ${ASSIGNMENTS_DIR} -name '*.json' -type f ); do
+for assignment in $(find ${ASSIGNMENTS_DIR} -name '*.json' -type f); do
 echo "Hello World $(assignment)"
   deploy_assignment "${assignment}"
 done
